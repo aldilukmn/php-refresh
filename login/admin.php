4436c4d83b1cc54679122d1265656ac1d7412c35
Aldi Lukman Maulana Latief <aldilukman99@gmail.com>
2023-04-30 20:14:18 +0700

    inheritance and overriding

--- login/admin.php
+++ login/admin.php
@@ -1,12 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Admin</title>
-    </head>
-    <body>
-        <h1>Selamat Datang, Admin</h1>
-        <a href="login.php">logout</a>
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+</head>
+
+<body>
+    <h1>Selamat Datang, Admin</h1>
+    <a href="login.php">logout</a>
+    <table border="2" cellspacing="2" cellpadding="4">
+        <tr>
+            <th colspan="4">Produk 1</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <td>martabak</td>
+            <td rowspan="3">sosis</td>
+            <td>mie</td>
+        </tr>
+        <tr>
+            <th>2</th>
+            <td>lontong</td>
+            <td>ayam penyet</td>
+        </tr>
+        <tr>
+            <th>3</th>
+            <td>bubur ayam</td>
+            <td>sop bayam</td>
+        </tr>
+    </table>
+</body>
+
 </html>
