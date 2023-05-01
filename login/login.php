cdd418667964e29d6e4851b40634088fe675ad05
Aldi Lukman Maulana Latief <aldilukman99@gmail.com>
2023-05-01 12:28:33 +0700

    implement simple login form

--- login/login.php
+++ login/login.php
@@ -1,7 +1,7 @@
 <?php
 
-if (isset($_POST['submit'])) {
-    if ($_POST['username'] == 'admin' && $_POST['password'] == 123) {
+if (isset($_POST['login'])) {
+    if ($_POST['username'] == 'admin' && $_POST['password'] == 'password') {
         header("Location: admin.php");
         exit;
     } else {
@@ -12,32 +12,35 @@ if (isset($_POST['submit'])) {
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Login</title>    
-    </head>    
-    <body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
 
-        <?php if(isset($error)) : ?>
-            <p style="color: red ; font-style: italic ;">Username & Password Salah</p>
-        <?php endif ?>    
-
-        <ul>
-            <form action="" method="post">
-                <li>
-                    <label for="username">username</label>
-                    <input type="text" id="username" name="username">
-                </li>
-                <li>
-                    <label for="password">password</label>
-                    <input type="text" id="password" name="password">
-                </li>
-                <button type="submi" name="submit">submit</button>
-            </form>
+<body>
+    <h1>Halaman Login</h1>
+    <?php if(isset($error)) : ?>
+        <p style="color:red; font-style:italic;">Username & Password salah! Coba lagi.</p>
+    <?php endif ?>
+    <form method="post">
+        <ul style="list-style: none;">
+            <li>
+                <label for="username">Username : </label>
+                <input type="text" id="username" name="username">
+            </li>
+            <br>
+            <li>
+                <label for="password">Password : </label>
+                <input type="password" id="password" name="password">
+            </li>
+            <li>
+                <button type="submit" name="login">Login</button>
+            </li>
         </ul>
-    </body>
+    </form>
+</body>
+
 </html>
